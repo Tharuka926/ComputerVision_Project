bfc6cb3125be736e110f196734935ce0bfe969f6
Tharuka926 <tath0004@student.monash.edu>
2026-04-02 13:20:24 +1100

    Add extra drawing capabilities

--- draw.py
+++ draw.py
@@ -7,10 +7,13 @@ print(img)
 # img[:] = 255,0,0 #Blue full image
 # img[200:300,100:300] = 255,0,0 #200:300 is height, 100:300 is width
 # cv2.line(img,(0,0),(300,300),(0,255,0),3) #(0,0) is starting point, (300,300) is ending point, (0,255) is colour, 3 is width
-cv2.line(img,(0,0),(img.shape[1],img.shape[0]),(0,255,0),3) # img.shape[1] is width, img.shape[0] is height
+cv2.line(img, (0,0), (img.shape[1], img.shape[0]), (0,255,0), 3) # img.shape[1] is width, img.shape[0] is height
 
-cv2.rectangle(img,(0,0),(250,350),(0,0,255), cv2.FILLED)
-cv2.circle(img,(400,50),30,(255,255,0),5)
+cv2.rectangle(img, (0,0), (250,350), (0,0,255), cv2.FILLED)
+cv2.circle(img,(400,50), 30, (255,255,0), 5)
+
+cv2.putText(img, " OPENCV ", (300, 200), cv2.FONT_HERSHEY_PLAIN, 1, (0,150,0), 2)
+
 
 cv2.imshow("Image", img)
 
