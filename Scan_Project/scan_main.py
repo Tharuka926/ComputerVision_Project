e1084af05b1756bced2e73acf2d0742c3a714000
Tharuka926 <tath0004@student.monash.edu>
2026-04-03 16:58:26 +1100

    Add document detection and primitve unwarp

--- Scan_Project/scan_main.py
+++ Scan_Project/scan_main.py
@@ -27,29 +27,77 @@ def preProcessing(img):
 
 
 def getContours(img):
+    biggest = np.array([])
+    maxArea = 0
+
     contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     for cnt in contours:
         area = cv2.contourArea(cnt)
-        print(area)
         # Make sure detected shape is bigger than 500 pixels
+        # area = 500
         if area > 500:
-            cv2.drawContours(imgContour, cnt, -1, (255, 0, 0), 3)
+            # cv2.drawContours(imgContour, cnt, -1, (255, 0, 0), 3)
             # peri = perimeter
             peri = cv2.arcLength(cnt, True)
-            # print(peri)
-            # approx = cv2.approxPolyDP(cnt, 0.05*peri, True)
             approx = cv2.approxPolyDP(cnt, 0.02*peri, True)
-            print(len(approx))
-            objCor = len(approx)
-            x, y, w, h, = cv2.boundingRect(approx)
+            
+            if (area > maxArea and len(approx) == 4):
+                biggest = approx
+                maxArea = area
 
+    cv2.drawContours(imgContour, biggest, -1, (255, 0, 0), 20)
+    
+    return biggest
+
+
+def reorder(myPoints):
+    myPoints = myPoints.reshape((4,2))
+    myPointsNew = np.zeros((4,1,2), np.int32)
+
+    add = myPoints.sum(1)
+    # print("add", add)
+
+    myPointsNew[0] = myPoints[np.argmin(add)]
+    myPointsNew[3] = myPoints[np.argmax(add)]
+
+    diff = np.diff(myPoints, axis = 1)
+    myPointsNew[1] = myPoints[np.argmin(diff)]
+    myPointsNew[2] = myPoints[np.argmax(diff)]
+
+    # print("NewPoints", myPointsNew)
+    return myPointsNew
+    
+
+def getWarp(img, biggest):
+
+    if (biggest.size == 0):
+        return img
+    
+    biggest = reorder(biggest)
+    # print(biggest)
+
+    pts1 = np.float32(biggest)
+    pts2 = np.float32([[0,0], [widthImg, 0], [0, heightImg], [widthImg, heightImg]])
+
+    matrix = cv2.getPerspectiveTransform(pts1, pts2)
+
+    imgOutput = cv2.warpPerspective(img, matrix, (widthImg, heightImg))
+
+    return imgOutput
 
 
 while True:
     success, img = cap.read()
     cv2.resize(img, (widthImg, heightImg))
+    imgContour = img.copy()
     imgThreshold = preProcessing(img)
-    cv2.imshow("Result", imgThreshold)
+
+    biggest = getContours(imgThreshold)
+    # print(biggest)
+    imgWarped = getWarp(img, biggest)
+
+
+    cv2.imshow("Result", imgWarped)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
