9163d7aa41604550b26424f1bc1df386f65ca942
Tharuka926 <tath0004@student.monash.edu>
2026-04-03 11:49:54 +1100

    Setup scanning project

---
+++ Scan_Project/scan_main.py
@@ -0,0 +1,19 @@
+import cv2
+
+# Import generic video
+# cap = cv2.VideoCapture("Resources/vid01.mp4")
+
+# Capture from webcam
+cap = cv2.VideoCapture(0) # id number for webcam (0)
+cap.set(3, 640) # id number for setting width of webcam (3)
+cap.set(4,480) # id number for setting height of webcam (4)
+cap.set(10,130) # id number for setting brightness of webcam (10)
+
+
+
+while True:
+    success, img = cap.read()
+    cv2.imshow("Video", img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
